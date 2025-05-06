75462b6aa0f9a6ff88e6e80b19000340f0e152af
John Hudson <aniwatchanime501@gmail.com>
2025-05-06 08:00:11 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
-import { limiter } from "./middlewares/rateLimit";
 import { router } from "./routes/routes";
 
 config(); // dotenv
 
 const app = express();
 const PORT = process.env.PORT ?? 3001;
-
-//middlewares
-app.use(limiter);
 
 // router
 app.use("/", router);
